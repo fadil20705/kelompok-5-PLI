63eca42437c197a734c777dd27654ca81792ef47
fadil20705 <aryafdhlk@gmail.com>
2025-04-17 10:52:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 MOBELEGEN")
+st.title("SEARCH YOUR FAVORITE BOOK")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
